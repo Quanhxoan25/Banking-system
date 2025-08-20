b0ad73096a320ad9af05a51fe88bf01955d83b2c
Quanhxoan25 <23020781@vnu.edu.vn>
2025-08-20 23:15:06 +0700

    Update Account and CheckingAccount

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.concurrent.TransferQueue;
 
-public class Account {
+public abstract class Account {
 
     public static final String CHECKING = "CHECKING";
     public static final String SAVINGS = "SAVINGS";
@@ -9,4 +9,50 @@ public class Account {
     private String id;
     private double balance;
     private List<Transaction> transactionList;
+
+    public Account() {}
+
+    public Account(String id, double balance, List<Transaction> transactions) {
+        this.id = id;
+        this.balance = balance;
+        this.transactionList = transactions;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setBalance(double balance) {
+        if (balance < 0) throw new IllegalArgumentException("So du khong hop le");
+        
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactionList() {
+        return transactionList;
+    }
+
+    public void setTransactionList(List<Transaction> transactionList) {
+        if (transactionList == null) throw new IllegalArgumentException("Danh sach giao dich rong");
+        this.transactionList = transactionList;
+    }
+
+    public abstract void deposit(double amount);
+    public abstract void withdraw(double amount);
+
+    public void doDeposit(double amount) {
+        balance += amount;
+    }
+
+    public void doWithdraw(double amount) {
+        balance = balance - amount;
+    }
 }
--- src/models/ChekingAccount.java
+++ src/models/ChekingAccount.java
@@ -1,3 +1,25 @@
-public class ChekingAccount {
+public class ChekingAccount extends Account {
     
+    public ChekingAccount() {
+        super();
+    }
+
+    public ChekingAccount(String id, double balance, List<Transaction> transactionList) {
+        super(id, balance, transactionList);
+    }
+
+    @Override
+    public void deposit(double amount) {
+        if (amount <= 0) throw new IllegalArgumentException("So tien nap vao khong hop le!");
+
+        super.doDeposit(amount);
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        double balance = getBalance();
+        if (amount <= 0 || amount > balance - amount) throw new IllegalArgumentException("So tien rut ra khong hop le!");
+
+        super.doWithdraw(amount);
+    }
 }
